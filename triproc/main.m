4a3f5b72486d862ecf8cd9c1f12b19082f28e4c5
Dmitry M <dmitrym@gmail.com>
2012-10-22 21:20:05 -0700

    Initial Commit

---
+++ triproc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  triproc
+//
+//  Created by Dmitry M on 2012-10-22.
+//  Copyright (c) 2012 Dmitry M. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
